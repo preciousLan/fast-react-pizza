b029c6aa02d4c1e755d46bf66c90c54f05a08394
precious lan <olanrewaju.faj@gmail.com>
2025-11-04 14:13:38 +0100

    updated logic

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,13 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 const initialState = {
-    cart: [{
-        pizzaId: 12,
-        name: "medetorian",
-        quantity : 2,
-        unitPrice: 16,
-        totalPrice: 32,
-    }],
+    cart: [],
     
 
 }
@@ -21,10 +15,9 @@ const cartSlice = createSlice({
 state.cart.push(action.payload)
     },
     deleteItem(state,action){
-        //payload = id
-        state.card = state.cart.filter(item => item.pizzaId !== action.payload)
-
-    },
+        //payload = pizzaId
+        state.cart = state.cart.filter(item => item.pizzaId !== action.payload)
+ },
     increaseItemQuantity(state,action){
         //payload: pizzaid
         const item = state.cart.find((item)=> item.pizzaId === action.payload)
@@ -45,5 +38,11 @@ item.totalPrice = item.quantity * item.unitPrice
 });
 
 export const {addItem,deleteItem,increaseItemQuantity,decreaseItemQuantity,clearCart} = cartSlice.actions
+
+
+export const getTotalCartQuantity = state => state.cart.cart.reduce((sum, item) => sum + item.quantity, 0)
+export const getTotalCartPrice = state => state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0)
+export const getCart = (state) => state.cart.cart
+export const getCurrentQuantityById = id => state => state.cart.cart.find(item=> item.pizzaId === id)?. quantity ?? 0;
 
 export default cartSlice.reducer
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,7 +1,31 @@
+import { useDispatch } from 'react-redux';
 import Button from '../../ui/Button';
 import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
+import { DeleteItem } from '../cart/DeleteItem';
+import { useSelector } from 'react-redux';
+
+
+
+
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch()
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+  const currentQuantity = useSelector(getCurrentQuantityById(id))
+  const isInCart = currentQuantity > 0
+
+  function handleAddToCart() {
+    const newItem = {
+      pizzaId: id,
+      name,
+      quantity: 1,
+      unitPrice,
+      totalPrice: unitPrice * 1
+    };
+    dispatch(addItem(newItem))
+
+  }
+
 
   return (
     <li className="flex gap-4 py-2">
@@ -23,8 +47,12 @@ function MenuItem({ pizza }) {
               Sold out
             </p>
           )}
+          {isInCart && <DeleteItem pizzaId={id}/>}
 
-          <Button type="small">Add to cart</Button>
+          {!soldOut && !isInCart &&
+            <Button type="small" onClick={handleAddToCart}>Add to cart</Button>
+
+          }
         </div>
       </div>
     </li>
